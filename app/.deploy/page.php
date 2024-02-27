fba224e1405c2b24c0bea3171041d0d9eb9b6134
robertkleinschuster <git@robs.social>
2024-02-27 18:24:45 +0100

    move deploy action to library

--- app/.deploy/page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Psr\Http\Message\ServerRequestInterface;
-use Slim\Exception\HttpMethodNotAllowedException;
-
-return fn(ServerRequestInterface $request) => throw new HttpMethodNotAllowedException($request);
